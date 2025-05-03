054f64b506ef5820a37da9b746adb423d31347b5
endulum <71853916+endulum@users.noreply.github.com>
2025-05-02 21:45:27 -0700

    complete linting for caesar cipher

--- basics/caesar_cipher.rb
+++ basics/caesar_cipher.rb
@@ -1,28 +1,30 @@
-def caesar_cipher(string, shift)
-  alphabet = Array("a".."z")
-  chars = string.chars
-  enciphered = []
-  chars.each do |char|
-    index = alphabet.index(char.downcase)
-    if index
-      # get new index
-      new_index = index + shift
-      if new_index.positive?
-        new_index -= 26 while new_index > 25
-      else
-        new_index += 26 while new_index.negative?
-      end
-
-      # get new char using index
-      new_char = alphabet[new_index]
-      new_char = new_char.upcase if char.upcase == char
-
-      # push!
-      enciphered.push(new_char)
-    else
-      enciphered.push(char)
-    end
+def correct_index(shift, index)
+  new_index = index + shift
+  if new_index.positive?
+    new_index -= 26 while new_index > 25
+  else
+    new_index += 26 while new_index.negative?
   end
+  new_index
+end
 
+def caesar_char(char, shift)
+  alphabet = Array("a".."z")
+  index = alphabet.index(char.downcase)
+  if index
+    new_index = correct_index(shift, index)
+    new_char = alphabet[new_index]
+    new_char = new_char.upcase if char.upcase == char
+    new_char
+  else
+    char
+  end
+end
+
+def caesar_cipher(string, shift)
+  chars = string.chars
+  enciphered = chars.map do |char|
+    caesar_char(char, shift)
+  end
   enciphered.join
 end
