bd6e6a492cfda87f614dc005aac296fc6d0c41e9
endulum <71853916+endulum@users.noreply.github.com>
2025-05-06 14:22:11 -0700

    add testing for `Colors` module

--- mastermind/lib/colors.rb
+++ mastermind/lib/colors.rb
@@ -10,41 +10,26 @@ module Colors
     "magenta" => :magenta
   }
 
-  # prints and colorizes a color string
-  def self.to_colorized_string(key)
-    key.colorize({ color: @colors[key] })
+  # all Mastermind colors
+  def self.all
+    @colors.keys
   end
 
-  # prints and colorizes all Mastermind colors in a sentence-appropriate phrase
+  # colorized string of all Mastermind colors in a sentence-appropriate phrase
   def self.all_to_colorized_string
-    @colors.keys[0..-2].map do |key|
+    all[0..-2].map do |key|
       to_colorized_string(key)
-    end.join(", ") + ", and #{to_colorized_string(@colors.keys[-1])}"
+    end.join(", ") + ", and #{to_colorized_string(all[-1])}"
   end
 
-  # asks for colors input
-  def self.prompt_colors
-    input_colors = Colors.parse_input_colors(CLI.read_input) until input_colors
-
-    # TODO: code to handle this as a "guess"
-    guess = input_colors.map do |c|
-      to_colorized_string(c)
-    end.join(", ")
-    CLI.print_bold("You guessed: #{guess}")
+  # color symbol of one Mastermind color
+  def self.to_symbol(string)
+    @colors[string]
   end
 
-  # parses input into Mastermind colors, or prints errors if input is invalid
-  def self.parse_input_colors(input)
-    array = input.split
-    is_four_colors_long = array.length == 4
-    is_all_valid_colors = array_of_colors?(array)
-    return array unless !is_four_colors_long || !is_all_valid_colors
-
-    messages = ["Errors with your guess: #{array}"]
-    messages.push("- You need to choose 4 colors. You've chosen #{array.length}.") unless is_four_colors_long
-    messages.push("- Please only use valid colors: #{Colors.all_to_colorized_string}") unless is_all_valid_colors
-    CLI.print_bold_all(messages)
-    nil
+  # colorized string of one Mastermind color
+  def self.to_colorized_string(string)
+    string.colorize({ color: to_symbol(string) }) if to_symbol(string)
   end
 
   # is given string a Mastermind color?
@@ -52,10 +37,37 @@ module Colors
     @colors.key?(string)
   end
 
-  # is given string array an array of Mastermind colors?
+  # is given array all Mastermind colors?
   def self.array_of_colors?(array)
     array.all? do |element|
       color?(element)
     end
   end
+
+  # MOVE BELOW TO OWN MODULE, MAYBE
+
+  # # asks for colors input
+  # def self.prompt_colors
+  #   input_colors = Colors.parse_input_colors(CLI.read_input) until input_colors
+
+  #   # TODO: code to handle this as a "guess"
+  #   guess = input_colors.map do |c|
+  #     to_colorized_string(c)
+  #   end.join(", ")
+  #   CLI.print_bold("You guessed: #{guess}")
+  # end
+
+  # # parses input into Mastermind colors, or prints errors if input is invalid
+  # def self.parse_input_colors(input)
+  #   array = input.split
+  #   is_four_colors_long = array.length == 4
+  #   is_all_valid_colors = array_of_colors?(array)
+  #   return array unless !is_four_colors_long || !is_all_valid_colors
+
+  #   messages = ["Errors with your guess: #{array}"]
+  #   messages.push("- You need to choose 4 colors. You've chosen #{array.length}.") unless is_four_colors_long
+  #   messages.push("- Please only use valid colors: #{Colors.all_to_colorized_string}") unless is_all_valid_colors
+  #   CLI.print_bold_all(messages)
+  #   nil
+  # end
 end
---
+++ mastermind/spec/colors_spec.rb
@@ -0,0 +1,75 @@
+require "spec_helper"
+require "colorize"
+require_relative "../lib/colors"
+
+all_colors = %w[red blue green yellow magenta]
+wrong_colors = %w[pink orange purple white black]
+
+describe "showing all colors" do
+  it "shows array of valid colors" do
+    expect(Colors.all).to eq all_colors
+  end
+
+  it "shows a colorized string of all valid colors" do
+    expect(Colors.all_to_colorized_string).to eq(
+      "#{
+        'red'.colorize({ color: :red })
+      }, #{
+        'blue'.colorize({ color: :blue })
+      }, #{
+        'green'.colorize({ color: :green })
+      }, #{
+        'yellow'.colorize({ color: :yellow })
+      }, and #{
+        'magenta'.colorize({ color: :magenta })
+      }"
+    )
+  end
+end
+
+describe "color string conversion" do
+  it "returns a symbol for each color, if a valid color" do
+    all_colors.each do |color|
+      expect(Colors.to_symbol(color)).to be_a(Symbol)
+    end
+    wrong_colors.each do |color|
+      expect(Colors.to_symbol(color)).to be_nil
+    end
+  end
+
+  it "returns a colorized string for each color, if a valid color" do
+    all_colors.each do |color|
+      expect(
+        Colors.to_colorized_string(color)
+      ).to eq(
+        color.colorize({ color: Colors.to_symbol(color) })
+      )
+    end
+    wrong_colors.each do |color|
+      expect(Colors.to_colorized_string(color)).to be_nil
+    end
+  end
+end
+
+describe "predicates" do
+  it "determines individual valid colors" do
+    all_colors.each do |color|
+      expect(Colors.color?(color)).to be true
+    end
+    wrong_colors.each do |color|
+      expect(Colors.color?(color)).to be false
+    end
+  end
+
+  it "determines if string array is all valid colors" do
+    expect(
+      Colors.array_of_colors?(%w[red blue])
+    ).to be true
+    expect(
+      Colors.array_of_colors?(%w[owo uwu])
+    ).to be false
+    expect(
+      Colors.array_of_colors?(%w[red pink white])
+    ).to be false
+  end
+end
