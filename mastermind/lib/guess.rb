659537ee964e2c1826a230f981d5cfe0b152f7ca
endulum <71853916+endulum@users.noreply.github.com>
2025-05-09 22:00:29 -0700

    extract guess making and checking into `Guess` class

---
+++ mastermind/lib/guess.rb
@@ -0,0 +1,61 @@
+require_relative "colors"
+require_relative "cli"
+
+# stores color combinations in "guesses" with feedback
+class Guess
+  def initialize
+    guess = parse_guess(CLI.read_input) until guess
+  end
+
+  # parse input into a guess, or return nothing and print errors if input is invalid
+  def self.parse(input)
+    array = input.split
+    is_four_colors_long = array.length == 4
+    is_all_valid_colors = Colors.array_of_colors?(array)
+    return array unless !is_four_colors_long || !is_all_valid_colors
+
+    print_guess_errors(is_four_colors_long, is_all_valid_colors) unless defined?(RSpec)
+    nil
+  end
+
+  # generate feedback from a guess based on the correct answer
+  def self.compare(guess, answer)
+    feedback_correct(guess, answer) + feedback_almost(guess, answer)
+  end
+
+  # find correct colors in correct spots
+  def self.feedback_correct(guess, answer)
+    feedback = []
+    guess.each_with_index do |color, index|
+      if color == answer[index]
+        feedback.push("correct")
+        answer[index] = nil
+      end
+    end
+    feedback
+  end
+
+  # find correct colors in wrong spots
+  def self.feedback_almost(guess, answer)
+    feedback = []
+    guess.each do |color|
+      if answer.include?(color)
+        feedback.push("almost")
+        answer[answer.find_index(color)] = nil
+      end
+    end
+    feedback
+  end
+
+  # collect guess errors into an array for printing
+  def print_guess_errors(is_four_colors_long, is_all_valid_colors)
+    messages = ["There were some errors with your guess, which was: #{array}"]
+    messages.push("- You need to choose 4 colors. You've chosen #{array.length}.") unless is_four_colors_long
+    messages.push("- Please only use valid colors: #{Colors.all_to_colorized_string}") unless is_all_valid_colors
+    CLI.print_all_unless_rspec(messages)
+  end
+
+  private :print_guess_errors
+  private_class_method :feedback_correct
+  private_class_method :feedback_almost
+end
---
+++ mastermind/spec/guess_spec.rb
@@ -0,0 +1,55 @@
+require "spec_helper"
+require "colorize"
+require_relative "../lib/colors"
+require_relative "../lib/guess"
+
+describe "making a guess" do
+  it "returns guess if valid guess string" do
+    expect(Guess.parse("red red red red")).to eq %w[red red red red]
+  end
+
+  it "returns nil if not enough colors" do
+    expect(Guess.parse("red red red")).to be nil
+  end
+
+  it "returns nil if too many colors" do
+    expect(Guess.parse("red red red red red")).to be nil
+  end
+
+  it "returns nil if unknown color" do
+    expect(Guess.parse("red blue pink orange")).to be nil
+  end
+
+  it "random tests" do
+    20.times do
+      colors = Colors.sample
+      expect(Guess.parse(colors.join(" "))).not_to be nil
+    end
+  end
+end
+
+describe "getting feedback from comparing guess to answer" do
+  it "works (all correct)" do
+    expect(
+      Guess.compare(%w[red red red red], %w[red red red red])
+    ).to eq %w[correct correct correct correct]
+  end
+
+  it "works (all almost)" do
+    expect(
+      Guess.compare(%w[red blue yellow green], %w[green yellow blue red])
+    ).to eq %w[almost almost almost almost]
+  end
+
+  it "works (some correct, some almost)" do
+    expect(
+      Guess.compare(%w[red blue red blue], %w[red blue blue red])
+    ).to eq %w[correct correct almost almost]
+  end
+
+  it "works (nothing)" do
+    expect(
+      Guess.compare(%w[red red red red], %w[blue blue blue blue])
+    ).to eq []
+  end
+end
