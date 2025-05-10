f9dc5ea77713a82695af9fafb5a4fb9067b481a1
endulum <71853916+endulum@users.noreply.github.com>
2025-05-10 15:40:55 -0700

    clean up main file

--- mastermind/main.rb
+++ mastermind/main.rb
@@ -1,20 +1,5 @@
 require_relative "lib/gameloop"
 require_relative "lib/cli"
 
-# def print_instructions
-#   CLI.print_bold_all(
-#     [
-#       "To make a guess, enter the names of the four colors of your guess.",
-#       "The available colors are #{Colors.all_to_colorized_string}.",
-#       "Proper format is each color separated by space, e.g. 'red blue red blue'."
-#     ]
-#   )
-# end
-
-# print_instructions
-
-# game = Gameplay.new
-# game.play
-
 game = Gameloop.new
 game.play
