d3890ef245f8b2d7b506b2d2a70b4d9a5d2d4278
endulum <71853916+endulum@users.noreply.github.com>
2025-05-10 20:03:08 -0700

    print guess feedback, extract method for creating guess attempt

--- mastermind/lib/gameloop.rb
+++ mastermind/lib/gameloop.rb
@@ -16,15 +16,24 @@ class Gameloop
   def play
     print_start
     loop do
-      attempt = Guess.new(@answer)
-      print_guess(attempt.guess)
-      @attempts.push attempt
-      print_table(@attempts)
-      next unless attempt.guess == @answer
-
-      print_win
-      break
+      attempt = make_attempt
+      if attempt.guess == @answer
+        print_win
+        break
+      else
+        print_feedback(attempt.feedback)
+      end
     end
+  end
+
+  private
+
+  def make_attempt
+    attempt = Guess.new(@answer)
+    print_guess(attempt.guess)
+    @attempts.push attempt
+    print_table(@attempts)
+    attempt
   end
 
   def print_start
@@ -43,13 +52,21 @@ class Gameloop
     end.join(', ')}".colorize({ mode: :bold })
   end
 
+  def print_feedback(feedback)
+    correct_count = feedback.count("correct")
+    almost_count = feedback.count("almost")
+    puts "You have #{correct_count} correct color#{
+      correct_count == 1 ? '' : 's'
+    } in the correct spot.".colorize({ mode: :bold })
+    puts "You have #{almost_count} correct color#{
+      almost_count == 1 ? '' : 's'
+    } in an incorrect spot.".colorize({ mode: :bold })
+  end
+
   def print_win
     puts "You guessed correctly! The code was #{@answer.map do |c|
       Colors.to_string(c)
     end.join(', ')}. You took #{@attempts.length} guesses."
        .colorize({ mode: :bold })
   end
-
-  private :print_start
-  private :print_win
 end
