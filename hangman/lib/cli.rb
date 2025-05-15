5580b9bc22817e0ed22d0d74e742ed7d0ba099bc
endulum <71853916+endulum@users.noreply.github.com>
2025-05-15 10:02:10 -0700

    suppress cli methods when testing

--- hangman/lib/cli.rb
+++ hangman/lib/cli.rb
@@ -2,15 +2,21 @@ require "colorize"
 
 # handles getting input and printing with the command line
 module CLI
+  def self.testing?
+    defined?(RSpec)
+  end
+
   def self.bprint(string)
-    puts string.colorize({ mode: :bold })
+    puts string.colorize({ mode: :bold }) unless testing?
   end
 
   def self.bprint_all(array)
-    print array.join("\n").colorize({ mode: :bold })
+    print array.join("\n").colorize({ mode: :bold }) unless testing?
   end
 
   def self.read_input(text = nil, prefix = "❯ ", color = :white)
+    raise "Should not be called in unit testing" if testing?
+
     print "\n#{text ? "#{text} " : ''}#{prefix}".colorize(color: color)
     gets.chomp
   end
