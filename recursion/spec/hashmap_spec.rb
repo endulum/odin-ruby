1b112729e172b17ae327c2ec76a91cc5ec42ca02
endulum <71853916+endulum@users.noreply.github.com>
2025-06-02 23:28:10 -0700

    tweak test description wording

--- recursion/spec/hashmap_spec.rb
+++ recursion/spec/hashmap_spec.rb
@@ -23,8 +23,8 @@ describe "hashing" do
   end
 end
 
-describe "individual getting and setting" do
-  it "should place a value as a list node" do
+describe "basics" do
+  it "#set: should place a value as a list node" do
     animals.each { |key, value| hashmap.set(key, value) }
     total = 0
     hashmap.each { |bucket| bucket&.each { total += 1 } }
@@ -33,15 +33,15 @@ describe "individual getting and setting" do
 
   target_animal = animals.keys[0]
 
-  it "should get a value by key" do
+  it "#get: should get a value by key" do
     expect(hashmap.get(target_animal)).to eq animals[target_animal]
   end
 
-  it "should not get a value for nonexistent key" do
+  it "#get: should not get a value for nonexistent key" do
     expect(hashmap.get("human")).to be_nil
   end
 
-  it "should overwrite a value" do
+  it "#set: should overwrite a value" do
     hashmap.set(target_animal, "none")
     expect(hashmap.get(target_animal)).to eq "none"
   end
