1689bd980aa85915c3fe6a604725ec7d8dd8c29f
endulum <71853916+endulum@users.noreply.github.com>
2025-06-05 22:14:02 -0700

    start tree class and create and test traversals

--- cs/lib/bst.rb
+++ cs/lib/bst.rb
@@ -20,4 +20,76 @@ module BST
       raise "Values #{@value} and #{other.value} could not be compared."
     end
   end
+
+  # Binary search tree
+  class Tree
+    def initialize(data)
+      add_data(data)
+    end
+
+    def add(value)
+      new_node = BST::Node.new(value)
+      if @root.nil?
+        @root = new_node
+      else
+        add_node(new_node, @root)
+      end
+    end
+
+    def preorder(node = @root, &)
+      yield node
+      preorder(node.left, &) if node.left
+      preorder(node.right, &) if node.right
+    end
+
+    def inorder(node = @root, &)
+      inorder(node.left, &) if node.left
+      yield node
+      inorder(node.right, &) if node.right
+    end
+
+    def postorder(node = @root, &)
+      postorder(node.left, &) if node.left
+      postorder(node.right, &) if node.right
+      yield node
+    end
+
+    def levelorder
+      queue = [@root]
+      visited = []
+      while queue.length.positive?
+        node = queue.shift
+        next unless node
+
+        yield node
+        visited.push(node)
+        queue.push(node.left) if node.left
+        queue.push(node.right) if node.right
+      end
+    end
+
+    def pretty_print(node = @root, prefix = "", is_left = true)
+      pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+      puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+      pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+    end
+
+    private
+
+    def add_data(data)
+      queue = data.dup
+      while queue.length.positive?
+        value = queue.pop
+        add(value)
+      end
+    end
+
+    def add_node(new_node, node)
+      if new_node > node
+        node.right.nil? ? node.right = new_node : add_node(new_node, node.right)
+      elsif new_node < node
+        node.left.nil? ? node.left = new_node : add_node(new_node, node.left)
+      end
+    end
+  end
 end
--- cs/spec/bst_spec.rb
+++ cs/spec/bst_spec.rb
@@ -9,3 +9,33 @@ describe "node comparison" do
     expect(two < one).to be false
   end
 end
+
+describe "recursion with block" do
+  random_numbers = (1..200).to_a.sample(15)
+  tree = BST::Tree.new(random_numbers)
+  tree.pretty_print
+
+  it "should recurse in preorder" do
+    preorder = []
+    tree.preorder { |node| preorder.push(node.value) }
+    expect(preorder).to match_array random_numbers
+  end
+
+  it "should recurse in postorder" do
+    postorder = []
+    tree.postorder { |node| postorder.push(node.value) }
+    expect(postorder).to match_array random_numbers
+  end
+
+  it "should recurse in inorder" do
+    inorder = []
+    tree.inorder { |node| inorder.push(node.value) }
+    expect(inorder).to eq random_numbers.sort
+  end
+
+  it "should iterate in levelorder" do
+    levelorder = []
+    tree.levelorder { |node| levelorder.push(node.value) }
+    expect(levelorder).to match_array random_numbers
+  end
+end
