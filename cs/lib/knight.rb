37730fa8327184e2f2a541e89f5b624053978593
endulum <71853916+endulum@users.noreply.github.com>
2025-06-06 23:04:47 -0700

    create and test method for determining next steps

--- cs/lib/knight.rb
+++ cs/lib/knight.rb
@@ -2,7 +2,26 @@
 module KnightsTravails
   extend self
 
-  def knight_moves
+  @diffs = [[-2, -1],
+            [-1, -2],
+            [2, 1],
+            [1, 2],
+            [-2, 1],
+            [-1, 2],
+            [2, -1],
+            [1, -2]]
+
+  def next_steps(square)
+    next_steps = []
+    @diffs.each do |x, y|
+      x_diff = square[0] + x
+      y_diff = square[1] + y
+      next_steps.push([x_diff, y_diff]) if x_diff.between?(0, 7) && y_diff.between?(0, 7)
+    end
+    next_steps
+  end
+
+  def steps(start_square, end_square)
     nil
   end
 end
--- cs/spec/knight_spec.rb
+++ cs/spec/knight_spec.rb
@@ -1,18 +1,30 @@
 require_relative "../lib/knight"
 
+describe "knight's paths" do
+  it "finds next steps for a knight" do
+    next_steps = [[2, 5], [5, 2], [1, 4], [4, 1], [4, 5], [5, 4], [2, 1], [1, 2]]
+    expect(KnightsTravails.next_steps([3, 3])).to match_array next_steps
+  end
+
+  it "skips out-of-bounds steps" do
+    next_steps = [[3, 7], [3, 5], [2, 4], [0, 4]]
+    expect(KnightsTravails.next_steps([1, 6])).to match_array next_steps
+  end
+end
+
 describe "knight's travails" do
   it "works with one step" do
-    expect(KnightsTravails.knight_moves([0, 0], [1, 2])).to eq [[0, 0], [1, 2]]
+    expect(KnightsTravails.steps([0, 0], [1, 2])).to eq [[0, 0], [1, 2]]
   end
 
   it "works with multiple steps" do
-    expect(KnightsTravails.knight_moves([0, 0], [3, 3])).to satisfy do |value|
+    expect(KnightsTravails.steps([0, 0], [3, 3])).to satisfy do |value|
       [[[0, 0], [2, 1], [3, 3]], [[0, 0], [1, 2], [3, 3]]].include?(value)
     end
-    expect(KnightsTravails.knight_moves([3, 3], [0, 0])).to satisfy do |value|
+    expect(KnightsTravails.steps([3, 3], [0, 0])).to satisfy do |value|
       [[[3, 3], [2, 1], [0, 0]], [[3, 3], [1, 2], [0, 0]]].include?(value)
     end
-    expect(KnightsTravails.knight_moves([0, 0], [7, 7])).to satisfy do |value|
+    expect(KnightsTravails.steps([0, 0], [7, 7])).to satisfy do |value|
       [[[0, 0], [2, 1], [4, 2], [6, 3], [4, 4], [6, 5], [7, 7]],
        [[0, 0], [2, 1], [4, 2], [6, 3], [7, 5], [5, 6], [7, 7]]].include?(value)
     end
